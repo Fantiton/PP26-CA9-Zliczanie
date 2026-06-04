686eb7c128088ca45e01d38875af3820dd29ad34
Filip Celejewski <FilipCelejewski@wp.pl>
2026-06-04 15:28:20 +0200

    Dodaj pliki projektów.

---
+++ PP26-CA9-Zliczanie/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rand = new Random();
+
+int[] array = new int[rand.Next(1, 100)];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-50, 50);
+}
+
+Console.WriteLine("Tablica:");
+foreach (int num in array)
+{
+    Console.WriteLine(" " + num);
+}
+Console.WriteLine();
