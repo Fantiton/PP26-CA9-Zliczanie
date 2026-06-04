508482e3848bde49fb1636d8de0c0bbd02c3cfb3
Filip Celejewski <FilipCelejewski@wp.pl>
2026-06-04 15:40:38 +0200

    Tworzenie tablicy pomocniczej

--- PP26-CA9-Zliczanie/Program.cs
+++ PP26-CA9-Zliczanie/Program.cs
@@ -13,3 +13,44 @@ foreach (int num in array)
     Console.WriteLine(" " + num);
 }
 Console.WriteLine();
+
+sort([1, 1, 1, 2, 2, 4, 8, 3, 4, 2, 5, 6, 7, 8, 9]);
+
+int[] sort(int[] inputArray)
+{
+    int highest = inputArray[0];
+    int lowest = inputArray[0];
+    for (int i = 1; i < inputArray.Length; i++)
+    {
+        if (inputArray[i] > highest) highest = inputArray[i];
+        if (inputArray[i] < lowest) lowest = inputArray[i];
+    }
+
+    int k = highest - lowest + 1;
+
+    int[] countArray = new int[k];
+
+    Console.WriteLine($"|ix| - |nb| - |ct|");
+    for (int i = lowest; i < highest; i++)
+    {
+        int count = 0;
+
+        for (int j = 0; j < inputArray.Length; j++)
+        {
+            if (inputArray[j] == i) count++;
+        }
+
+        if (i == lowest)
+        {
+            countArray[0] = count;
+            Console.WriteLine($"|{i - lowest}| - |{i}| - |{count}|");
+        }
+        else
+        {
+            countArray[i - lowest] = count + countArray[i - lowest - 1];
+            Console.WriteLine($"|{i - lowest}| - |{i}| - |{count + countArray[i - lowest - 1]}|");
+        }
+    }
+
+    return [1, 2];
+}
